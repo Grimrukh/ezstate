2a837f6fb5603921abb86b3f3a5205eeda0f35b8
grimrhapsody <33849304+grimrhapsody@users.noreply.github.com>
2017-11-26 02:11:02 -0500

    Initial commit.
    Only works for /talk/ .esd files.

---
+++ ezstate_parser.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+"""
+@author: grimrhapsody
+"""
+
+from struct import unpack
+
+
+REGISTERS = [''] * 8
+
+
+def reset_registers():
+    global REGISTERS
+    REGISTERS = [''] * 8
+
+
+marker_lookup = {
+    '91': '<=',
+    '92': '>=',
+    '93': '<',
+    '94': '>',
+    '95': '==',
+}
+
+function_lookup = {
+    # Not all of these are confirmed. Feel free to study the code and present evidence.
+    # Note that these indices are completely different to the on/off command IDs.
+    1:  'GetPlayerDistance',
+    2:  'IsDialogueFinished',
+    4:  'GetActionButton',
+    5:  'DamagedByPlayer',
+    6:  'HealthPercentage',  # from 0 to 100.
+    8:  'PlayerFacingAngle',  # angle away from the NPC that player is facing. Max is 180.
+    9:  'IsFriendly',
+    14: 'GetPromptState',
+    15: 'GetEventFlagState',  # takes an event flag as an argument.
+}
+
+
+def parse(input_line):
+    # input_line is a list of hex byte strings.
+
+    global REGISTERS
+
+    output_line = []
+    offset = 0
+    condition_start = 0
+
+    while offset < len(input_line):
+
+        byte = input_line[offset]
+        offset += 1
+
+        if '3f' <= byte <= '7f':
+            # Interpret byte as an integer offset by +64.
+            output_line.append(str(int(byte, 16) - 64))
+
+        else:
+
+            if byte == 'a5':
+                # This byte appears at the start of arguments to command (0), often at the start of the ON command of
+                # the main waiting state. I haven't figured out what data type it represents yet, so I'm ignoring the
+                # rest of the line for now.
+                return ' '.join(input_line)
+
+            elif byte == '80':
+                # Next four bytes form a float.
+                float_value = unpack('<f', bytearray.fromhex(''.join(input_line[offset:offset + 4])))[0]
+                offset += 4
+                output_line.append(str(float_value))
+
+            # Byte '81' represents another data type I haven't identified, but doesn't appear often in the talk files.
+
+            elif byte == '82':
+                # Next four bytes form an integer.
+                integer = unpack('<i', bytearray.fromhex(''.join(input_line[offset:offset + 4])))[0]
+                offset += 4
+                output_line.append(str(integer))
+
+            # I haven't seen byte '83' yet but it's safe to say it will indicate a data type - maybe a string.
+
+            elif byte == '84':
+                # Previous integer is a function index with no arguments.
+                function_index = int(output_line[-1])
+                function_name = function_lookup.get(function_index, 'F-{}'.format(function_index))
+                output_line[-1] = function_name + '()'
+
+            elif byte == '85':
+                # Previous two values represent a function index and one argument (in that order).
+                function_index = int(output_line[-2])
+                function_name = function_lookup.get(function_index, 'F-{}'.format(function_index))
+                output_line[-2] = function_name + '({})'.format(output_line[-1])
+                output_line.pop()   # function and argument have been combined
+
+            elif byte == '86':
+                # Previous three values represent a function index and two arguments (in that order).
+                function_index = int(output_line[-3])
+                function_name = function_lookup.get(function_index, 'F-{}'.format(function_index))
+                output_line[-3] = function_name + '({}, {})'.format(output_line[-2], output_line[-1])
+                output_line.pop()   # function and arguments have been combined
+                output_line.pop()
+
+            # I assume bytes 87-90 mark functions that take more arguments but haven't encountered them yet to confirm.
+
+            elif '91' <= byte <= '95':
+                # Applies comparison operator to the last two values (left and right, respectively).
+                comparison_operator = marker_lookup[byte]
+                output_line[-2] = '({} {} {})'.format(output_line[-2], comparison_operator, output_line[-1])
+                output_line.pop()
+
+            elif byte == '98':
+                # Applies AND operation to the last two values.
+                output_line[-2] = '{} and {}'.format(output_line[-2], output_line[-1])
+                output_line.pop()
+
+            elif byte == '99':
+                # Applies OR operation to the last two values.
+                output_line[-2] = '{} or {}'.format(output_line[-2], output_line[-1])
+                output_line.pop()
+
+            elif byte == 'a1':
+                # End of line. Not printed. Note that it is NOT responsible for registering the state change conditions,
+                # as it appears in command argument data as well. It's strictly an end line marker. Also note that it
+                # can appear elsewhere in the code, such as in the value of an integer or float, and it is therefore NOT
+                # safe to blindly parse the packed data based solely on this byte.
+                pass
+
+            elif byte == 'a6':
+                # My leading hypothesis for this byte is that it forces the command to continue even when the previous
+                # value is false (0), which would normally stop the condition line from continuing. I have left this
+                # byte out of the normal display mode, but you can enable it (and b7) with show_continuation=True.
+                output_line[-1] += '^'
+                pass
+
+            elif 'a7' <= byte <= 'ae':
+                # Save previous value to register.
+                REGISTERS[int(byte, 16) - 167] = output_line[-1]
+
+            elif 'af' <= byte <= 'b6':
+                # Load value from register. The ampersand indicates that this value has been computed earlier in the
+                # state conditions, which ensures that values do not change during a single evaluation of the conditions
+                # for each potential state change.
+                output_line.append('&'+REGISTERS[int(byte, 16) - 175])
+
+            elif byte == 'b7':
+                # My leading hypothesis for this byte is that it stops the current line from continuing evaluation if
+                # the previous value is false (0). It usually occurs later in the state because early conditions often
+                # *must* finish evaluating to save computed values for later loading. It also occurs in situations like
+                # "1 and (2 or 3 or 4)", when the AND command does not occur until the end of the command.
+                #
+                # Obviously, this hypothesis isn't fully consistent with my hypothesis about 'a6', which assumed that
+                # values of 0 can halt a command. It's possible that the developers occasionally used these continuation
+                # instructions superfluously. Or I could be wrong about one or both of them.
+                output_line[-1] += '!'
+
+            else:
+                marker = marker_lookup.get(byte, None)
+                if marker is None:
+                    output_line.append('[{}]'.format(byte))
+                else:
+                    output_line.append(marker)
+
+    return ' '.join(output_line)
---
+++ unpack_esd.py
@@ -0,0 +1,291 @@
+# -*- coding: utf-8 -*-
+"""
+@author: grimrhapsody
+
+Current only works for .esd files in \talk\ (NPCs and bonfires). Does not work for \menu\ or \chr\ .esd files.
+"""
+
+from contextlib import redirect_stdout
+from struct import unpack
+
+import ezstate_parser
+
+
+command_names = {
+
+    # Names of functions called from the command table.
+
+    -1: '(NULL)',             # no command
+    1: 'Play Dialogue',       # (text_id)    matches sound file ID, I believe
+    6: 'Action Prompt',       # (text_id)    -1 == no prompt
+    11: 'Set Event Flag',     # (event_flag_id, state)
+
+    # Unknown indices are left as "Unknown (index)". Note that the largest command index I've seen is 67.
+}
+
+
+def unpack_tables(filename):
+
+    file_header_length = 27 * 4   # 108 bytes at the start of the .esd file
+
+    # 1. Directly unpack all six sections of data, replacing byte offsets with table row indices (except for the
+    #    packed data table).
+
+    with open(filename, 'rb') as file:
+        
+        # The header contains structural information about the file.
+        data = file.read(file_header_length)
+        file_header_format = '<' + 'i' * (file_header_length // 4)
+        file_header = list(unpack(file_header_format, data))
+
+        # Offset 0 starts here.
+        if filename in ['enemyCommon', 'dummy']:
+            state_table_header_length = 19 * 4   # information about double state table, 76 bytes
+        else:
+            state_table_header_length = 15 * 4   # information about single state table, 60 bytes
+
+        state_table_offset = 0 + state_table_header_length
+        state_table_rows = file_header[12]   # number of states
+        state_table_width = file_header[11]   # bytes per row
+        state_table_format = '<iiiiiiiii'
+        
+        next_state_table_offset = state_table_offset + (state_table_rows * state_table_width)
+        next_state_table_rows = file_header[14]
+        next_state_table_width = file_header[13]
+        next_state_table_format = '<iiiiiii'
+        
+        command_table_offset = next_state_table_offset + (next_state_table_rows * next_state_table_width)
+        command_table_rows = file_header[16]
+        command_table_width = file_header[15]
+        command_table_format = '<iiii'
+        
+        command_arg_table_offset = command_table_offset + (command_table_rows * command_table_width)
+        command_arg_table_rows = file_header[18]
+        command_arg_table_width = file_header[17]
+        command_arg_table_format = '<ii'
+
+        # This offset is also given in the header (offset 19).
+        next_state_pointer_table_offset = command_arg_table_offset + (command_arg_table_rows * command_arg_table_width)
+        next_state_pointer_table_rows = file_header[20]
+        next_state_pointer_table_width = 4
+        next_state_pointer_table_format = '<i'
+        
+        # The remainder of the file is packed data, which is used as command arguments and conditions. It is accessed
+        # using a byte offset and byte length. Each command line ends with byte `\xa1`, but this byte can also appear
+        # earlier in the line (e.g. as part of a four-byte integer or float), so it's not safe to unpack the data here
+        # using this end-line marker.
+        packed_data_offset = next_state_pointer_table_offset + (next_state_pointer_table_rows * next_state_pointer_table_width)
+
+        # State table has a header describing its length. More complex .esd files, like enemyCommon.esd, have TWO
+        # state tables, and the length of each is described here. Note that for this reason, enemyCommon and c0000 have
+        # a 19-byte state table header instead of a 15-byte header. For now, just skipping these bytes.
+        file.read(state_table_header_length)
+        
+        state_table = []
+        for r in range(state_table_rows):
+            row = list(unpack(state_table_format, file.read(state_table_width)))
+            # Convert pointer to table 5.
+            if row[1] != -1:
+                row[1] = (row[1] - next_state_pointer_table_offset) // next_state_pointer_table_width
+                # row[2] is already just the number of rows to get.
+            # Convert pointers to table 3.
+            for i in [3, 5]:
+                if row[i] != -1:
+                    row[i] = (row[i] - command_table_offset) // command_table_width
+            state_table.append(row)
+    
+        next_state_table = []
+        for r in range(next_state_table_rows):
+            row = list(unpack(next_state_table_format, file.read(next_state_table_width)))
+            # Convert pointer to table 1.
+            if row[0] != -1:
+                row[0] = (row[0] - state_table_offset) // state_table_width
+            # Convert pointer to table 3.
+            if row[1] != -1:
+                row[1] = (row[1] - command_table_offset) // command_table_width
+            # Convert pointer to table 5.
+            if row[3] != -1:
+                row[3] = (row[3] - next_state_pointer_table_offset) // next_state_pointer_table_width
+            # Convert pointer to table 6 (still byte index).
+            if row[5] != -1:
+                row[5] = (row[5] - packed_data_offset)
+            next_state_table.append(row)
+            
+        command_table = []
+        for r in range(command_table_rows):
+            row = list(unpack(command_table_format, file.read(command_table_width)))
+            # Convert pointer to table 4.
+            if row[2] != -1:
+                row[2] = (row[2] - command_arg_table_offset) // command_arg_table_width
+            command_table.append(row)
+            
+        command_arg_table = []
+        for r in range(command_arg_table_rows):
+            row = list(unpack(command_arg_table_format, file.read(command_arg_table_width)))
+            # Convert pointer to table 6.
+            if row[0] != -1:
+                row[0] = (row[0] - packed_data_offset)
+            command_arg_table.append(row)
+
+        next_state_pointer_table = []
+        for r in range(next_state_pointer_table_rows):
+            row = list(unpack(next_state_pointer_table_format, file.read(next_state_pointer_table_width)))
+            # Convert pointer to table 2.
+            if row[0] != -1:
+                row[0] = (row[0] - next_state_table_offset) // next_state_table_width
+            next_state_pointer_table.append(row)
+
+        packed_data = []
+        data = file.read(1).hex()
+        while data != '':
+            packed_data.append(data)
+            data = file.read(1).hex()
+
+    return [state_table, next_state_table, command_table, command_arg_table, next_state_pointer_table, packed_data]
+
+
+def resolve_tables(state_table, next_state_table, command_table,
+                   command_arg_table, next_state_pointer_table, packed_data):
+    """ Resolve next state pointers and packed data pointers, leaving just the state, next state, and command tables.
+    """
+
+    for row in state_table:
+        # Note: does not remove the trailing (-1, 0) fields on the off chance that they are used at some rare point.
+
+        off_commands = [row[5] + i for i in range(row[6])]
+        row[5] = off_commands
+        row.pop(6)
+
+        on_commands = [row[3] + i for i in range(row[4])]
+        row[3] = on_commands
+        row.pop(4)
+
+        next_states = [next_state_pointer_table[row[1] + i][0] for i in range(row[2])]
+        row[1] = next_states
+        row.pop(2)
+
+    for row in next_state_table:
+
+        row[5] = packed_data[row[5]:row[5]+row[6]]
+        row.pop(6)
+
+        next_states = [next_state_pointer_table[row[3] + i][0] for i in range(row[4])]
+        row[3] = next_states
+        row.pop(4)
+
+        commands = [row[1] + i for i in range(row[2])]
+        row[1] = commands
+        row.pop(2)
+
+    for row in command_table:
+        args = []
+        for arg_index in range(row[3]):
+            arg_offset, arg_length = command_arg_table[row[2]+arg_index]
+            args.append(packed_data[arg_offset:arg_offset+arg_length])
+        row[2] = args
+        row.pop()
+
+    # Final formats. The starred fields in the next state table rarely appear (never in talk files) and I don't yet
+    # understand their purpose. They may take effect if the condition evaluates to false (so it runs commands and/or
+    # checks additional conditions for a new set of potential next states contingent on the first condition failing).
+
+    # State table:      [ state_index,  [next_state_indices], [on_command_indices],  [off_command_indices] ]
+    # Next state table: [ state_index, *[command_indices],   *[next_state_indices],  condition             ]
+    # Command table:    [ 1,            command_id,           [command_args]      ]
+
+    return [state_table, next_state_table, command_table]
+
+
+def write_tables(output, tables):
+    """ Write list of input tables to a text file, row by row. """
+    with open(output+'.txt', 'w') as file:
+        for table in tables:
+            for row in table:
+                file.write(str(table)+'\n')
+            file.write('\n')
+
+
+def write_state_title_bar(index):
+
+    print()
+    print('#######################{}'.format('#'*len(str(index))))
+    print('# EzState Index:   {}   #'.format(index))
+    print('#######################{}'.format('#'*len(str(index))))
+    print()
+
+
+def display_esd(state_table, next_state_table, command_table, rows=None, raw=False):
+    
+    # Display ESD files in a more readable format.
+    
+    if rows is None:
+        rows = range(len(state_table))
+
+    print("NOTES:\n"
+          "  - Including all logic grouping brackets is ugly, so I have disabled them by default. It is generally\n"
+          "    safe to assume that logical operations evaluate from left to right when they are all one type, and\n"
+          "    that later OR operations are evaluated before earlier AND operations. Set full_brackets=True for\n"
+          "    explicit order.\n"
+          "  - &: values that have been previously computed in the current condition evaluation for this state and\n"
+          "    loaded from registers.\n"
+          "  - ^: interpreter should continue even if the previous value is false.\n"
+          "  - !: interpreter should stop if the previous value is false. (Yes, this is not logically consistent\n"
+          "    with the above, but I'm not certain exactly what makes the interpreter halt during a line. It may\n"
+          "    halt whenever a zero value is not saved to a register, hence why this 'null register' is used.)\n")
+
+    for row in rows:
+
+        state_index = state_table[row][0]
+        next_states = [next_state_table[i] for i in state_table[row][1]]
+        on_commands = [command_table[i] for i in state_table[row][2]]
+        off_commands = [command_table[i] for i in state_table[row][3]]
+
+        write_state_title_bar(state_index)
+        
+        print('(ON)  Commands: ')
+        for _, id, args in on_commands:
+            name = command_names.get(id, 'Unknown ({})'.format(id))
+            if raw:
+                print('    {}: {}({})'.format(name, ' ' * (20 - len(name)),
+                                              ', '.join([' '.join(arg) for arg in args])))
+            else:
+                print('    {}: {}({})'.format(name, ' '*(20-len(name)),
+                                              ', '.join([ezstate_parser.parse(arg) for arg in args])))
+        print()
+        print('Next State Conditions:')
+        ezstate_parser.reset_registers()
+        for next_state_index, alt_commands, alt_next_states, condition in next_states:
+            print('  ---> State {}:'.format(next_state_index))
+            if alt_commands:
+                print('     ALT Commands: ')
+            for _, id, args in alt_commands:
+                name = command_names.get(id, 'Unknown ({})'.format(id))
+                if raw:
+                    print('    {}: {}({})'.format(name, ' ' * (20 - len(name)),
+                                                  ', '.join([' '.join(arg) for arg in args])))
+                else:
+                    print('    {}: {}({})'.format(name, ' ' * (20 - len(name)),
+                                                  ', '.join([ezstate_parser.parse(arg) for arg in args])))
+            if alt_next_states:
+                print('     ALT Next States: {}'.format(alt_next_states))
+            if raw:
+                print('   {}'.format(' '.join(condition)))
+            else:
+                print('     {}'.format(ezstate_parser.parse(condition)))
+        print()
+        print('(OFF)  Commands: ')
+        for _, id, args in off_commands:
+            name = command_names.get(id, 'Unknown ({})'.format(id))
+            print('    {}: {}({})'.format(name, ' ' * (20 - len(name)),
+                                          ', '.join([ezstate_parser.parse(args) for args in args])))
+
+if __name__ == '__main__':
+
+    in_filename = 'talk/t100000.esd'   # .esd file path
+    out_filename = 't100000_interpreted.txt'   # output file path
+    tables = unpack_tables(in_filename)   # raw unpacked tables (with offsets converted to row indices)
+    tables = resolve_tables(*tables)   # interpreted tables reduced to state table, next state table, and command table
+
+    with open(out_filename, 'w') as file:
+        with redirect_stdout(file):
+            display_esd(*tables, raw=False)
